468facfeef57ac1f8b4af91422b240b3a1ecd844
sajjad <sajjad.taghi.84.2005@gmail.com>
2025-07-26 20:43:11 +0330

    edit some magic value

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,15 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include "includes.h"
+
+
 const char CSV_DELIMITER = ',';
 const char LIST_DELIMITER = ';';
+const std::string PERFECT = "Perfect";
+const std::string AVERAGE = "Average";
+const std::string POOR = "Poor";
+const std::string NO_RATING = "";
+
 
 #endif 
--- src/Rating.cpp
+++ src/Rating.cpp
@@ -1,16 +1,18 @@
 #include "Rating.h"
+#include "constants.h"
+
 
 Rating string_to_rating(const std::string& str) {
-    if (str == "Perfect") return Rating::Perfect;
-    if (str == "Average") return Rating::Average;
+    if (str == PERFECT) return Rating::Perfect;
+    if (str == AVERAGE) return Rating::Average;
     return Rating::Poor;
 }
 
 std::string rating_to_string(Rating r) {
     switch (r) {
-        case Rating::Perfect: return "Perfect";
-        case Rating::Average: return "Average";
-        case Rating::Poor: return "Poor";
+        case Rating::Perfect: return PERFECT;
+        case Rating::Average: return AVERAGE;
+        case Rating::Poor: return POOR;
     }
-    return "";
+    return NO_RATING;
 }
